6ff28ca164d178dcfb25f9e3eb08c3779348e97c
Isack23 <isacklopes22@gmail.com>
2022-10-04 22:00:18 -0300

    Aprendendo JavaScript

--- aula06A/ex001.js
+++ aula06A/ex001.js
@@ -1,5 +1,5 @@
 var n1 = Number.prompt('Digite um numero: ')
 var n2 = Number.prompt('Digite outro numero: ')
 var soma = n1 + n2
-alert('A soma é :' + soma)
+alert(`A soma entre ${n1} e ${n2} é igual a ${soma}`)
 
---
+++ aula06B/ex004.js
@@ -0,0 +1,4 @@
+var nome = prompt('Digite seu nome: ')
+document.writeln(`<h2>Olá ${nome} ! Seu nome tem ${nome.length} letras.</h2>` ) 
+document.writeln(`Seu nome em maiusculo é ${nome.toUpperCase()}`)
+document.write(`seu nome em minusculo é ${nome.toLowerCase()}`)
