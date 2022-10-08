8da93f4795ca1ca56213c6c6960e0fb78d76747f
Isack23 <isacklopes22@gmail.com>
2022-10-08 01:06:38 -0300

    alterando css

--- aula10/ex007.js
+++ aula10/ex007.js
@@ -1,11 +1,14 @@
 function media() {
-    var n = document.getElementById("nome")
+    var nome = document.getElementById("nome")
     var tn1 = document.getElementById('txtn1')
     var tn2 = document.getElementById('txtn2')
     var res = document.getElementById('res')
     var n1 = Number(tn1.value)
     var n2 = Number(tn2.value)
+    
+     
     var media = (n1 + n2) / 2 
-    res.innerHTML = `${n} Sua media é: <strong>${media}</strong>`
+    res.innerHTML = ` Sua media é: <strong>${media}</strong>`
+    
 
 }
