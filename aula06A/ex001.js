b725e317b1ef37320bbd27c74248474803228bc2
Isack23 <isacklopes22@gmail.com>
2022-10-13 21:14:26 -0300

    atualizando a aula 6

--- aula06A/ex001.js
+++ aula06A/ex001.js
@@ -1,5 +1,7 @@
-var n1 = Number.prompt('Digite um numero: ')
-var n2 = Number.prompt('Digite outro numero: ')
-var soma = n1 + n2
-alert(`A soma entre ${n1} e ${n2} é igual a ${soma}`)
+
+var n1 = Number(prompt('Digite um numero: '))
+var n2 = Number(prompt('Digite outro numero: '))
+var media = (n1 + n2)/2
+document.write("A media é: " + media)
+
 
